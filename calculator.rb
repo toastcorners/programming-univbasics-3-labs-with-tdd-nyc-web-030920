26d8796129669d833b0f53e83d30578e2d90898c
Melissa Wise <wise.melissa19@gmail.com>
2020-02-27 05:07:16 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,2 +1,7 @@
 # Add your variables here
-first_number = "Hello World!"
+first_number = 5
+second_number = 10
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
